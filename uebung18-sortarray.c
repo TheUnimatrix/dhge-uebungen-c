687beffe8b5ebe7ec65edad537bb25b34f3a260a
TheUnimatrix <sebastian.girke@yahoo.de>
2024-12-24 14:32:15 +0100

    Programm (Übung 18) hinzugefügt

---
+++ uebung18-sortarray.c
@@ -0,0 +1,18 @@
+/*
+ * Arrays (Sortieren, einfügen und löschen von Zahlen)
+ * 
+ * Sebastian Girke, PIA24
+ * 2024-12-24
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_SIZE 10
+
+int main(int argc, char **argv) {
+	
+	
+	
+	return EXIT_SUCCESS;
+}
