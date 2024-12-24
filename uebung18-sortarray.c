5a532970bcd23b6bc056c37bb22ae857e79493d4
TheUnimatrix <sebastian.girke@yahoo.de>
2024-12-24 20:28:11 +0100

    Übung 18 vervollständigt

--- uebung18-sortarray.c
+++ uebung18-sortarray.c
@@ -7,24 +7,77 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define MAX_SIZE 10
 
 /** Methode zum Hinzufügen eines neuen Wertes in ein Array */
-void addValueToArray(int *array, int value, int anz) {
+void addValueToArray(int *array, int value, int *anz) {
 	
 	// Überprüfe aktuelle Anzahl des Arrays
-	if (anz == 0) {
+	if (*anz == 0) {
 		array[0] = value;
 	} else {
-		// Suche nach der Stelle, wo das Element eingefügt werden kann
-		for (int i = 0; i < anz; ++i) {
-			if (array[i] > value || i == anz) array[i] = value;
+		
+		int pos = -1; // Position, an der Element eingefügt werden kann
+		
+		// Suche nach der Position, wo das Element eingefügt werden kann
+		for (int i = 0; i <= *anz; ++i) {
+			if (array[i] > value || i == *anz) {
+				pos = i;
+				break;
+			}
+		}
+		
+		// Überprüfe, ob das neue Element an die letzte Position soll
+		if (pos == *anz) {
+			array[pos] = value;
+		} else {
+		
+			// Verschiebe alle Elemente nach rechts,
+			// um Wert hinzuzufügen
+			for (int i = *anz; i > pos; --i) {
+				array[i] = array[i - 1];
+			}
+			
+			array[pos] = value;
 		}
 	}
+	
+	++(*anz);
 }
 
-int main() {
+/** Methode zum Löschen eines Wertes aus einem Array */
+bool delValueFromArray(int *array, int value, int *anz) {
+	
+	int pos = -1;
+	
+	// Suche das zu löschende Element
+	for (int i = 0; i <= *anz; ++i) {
+		
+		if (array[i] > value) {
+			// Abbruch der Schleife; Element wurde nicht gefunden
+			return false;
+		}
+		
+		if (array[i] == value) {
+			pos = i; // Abbruch der Schleife; Element wurde gefunden
+			break;
+		}
+	}
+	
+	// Verschiebe alle Elemente nach vorne,
+	// um das zu löschende Element zu überschreiben
+	for (int i = pos; i < *anz; ++i) {
+		array[i] = array[i + 1];
+	}
+	
+	--(*anz);
+	
+	return true;
+}
+
+int main(int argc, char **argv) {
 	
 	int zahlen[MAX_SIZE]; // Array
 	int anz = 0; // Anzahl gespeicherter Zahlen
@@ -51,35 +104,52 @@ int main() {
 		}
 		
 		// Überprüfe, ob die maximale Größe des Arrays erreicht wurde
-		if (anz == MAX_SIZE) {
-			printf("Maximalgröße des Arrays (%d) erreicht! Es können "
-				"keine weiteren Zahlen hinzugefügt werden. Bitte erst "
-				"eine Zahl löschen!\n", MAX_SIZE);
+		if (input > 0 && anz == MAX_SIZE) {
+			fprintf(stderr, "%s: Maximalgröße des Arrays (%d) "
+				"erreicht! Es können keine weiteren Zahlen hinzugefügt "
+				"werden. Bitte erst eine Zahl löschen!\n\n", argv[0],
+				MAX_SIZE);
+			continue; // Fortfahren der Schleife
+		}
+		// Überprüfe, ob direkt eine Zahl gelöscht werden soll,
+		// obwohl das Array noch keine Elemente enthält
+		else if (input < 0 && anz == 0) {
+			fprintf(stderr, "%s: Das Array besitzt noch keinerlei "
+				"Elemente! Bitte erst Zahlen hinzufügen!\n\n", argv[0]);
 			continue; // Fortfahren der Schleife
 		}
 		
 		// Wenn eingegebene Zahl größer als 0 ist, dann füge diese hinzu
 		if (input > 0) {
-			addValueToArray(zahlen, input, anz++);
+			addValueToArray(zahlen, input, &anz);
 		}
 		// Wenn eingegebene Zahl kleiner als 0 ist, dann lösche diese
 		else if (input < 0) {
 			int del = abs(input); // der zu löschende Wert
+			bool result = delValueFromArray(zahlen, del, &anz);
+			
+			// Überprüfe, ob Wert gelöscht werden konnte
+			// (d.h. Wert wurde im Array gefunden)
+			if (!result) {
+				printf("FEHLER: Der angegebene Wert '%d' konnte nicht "
+					"gefunden werden!\n", del);
+			}
 		}
 		
 		// Ausgabe des Arrays
 		printf("Array: ");
-		for (int i = 0; i < anz; ++i) {
-			printf("%d ", zahlen[i]);
+		
+		if (anz == 0) { // passiert, wenn alle Elemente gelöscht wurden
+			printf("(leer)");
+		} else {
+			// Ausgabe vorhandener Elemente
+			for (int i = 0; i < anz; ++i) {
+				printf("%d ", zahlen[i]);
+			}
 		}
 		
 		printf("\n\n"); // Leerzeile
 	}
 	
 	return EXIT_SUCCESS; // Programmende
-}
-
-
-
-void delValueFromArray(int array[], int value) {
 }
